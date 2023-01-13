a20e244e03ccb0d58f5a198a61ea38db8d4e3c95
muac404 <muac404@gmail.com>
2023-01-13 18:50:40 +0100

    Aggiunge nav + hero

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,11 @@
+@extends("layouts.main")
+@section('page-title')
+    Homepage
+@endsection
+
+@section("page-content")
+<section>
+    <h2></h2>
+</section>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,21 @@
+<header>
+    <div class="container d-flex justify-content-center align-items-center py-3">
+        <img class="img-fluid" src="{{Vite::asset("resources/img/dc-logo.png")}}" alt="logo">
+        <ul class="d-flex">
+            <li class="list-unstyled px-3"><a href="#">CHARACTERS</a></li>
+            <li class="list-unstyled px-3"><a href="#">COMICS</a></li>
+            <li class="list-unstyled px-3"><a href="#">MOVIES</a></li>
+            <li class="list-unstyled px-3"><a href="#">TV</a></li>
+            <li class="list-unstyled px-3"><a href="#">GAMES</a></li>
+            <li class="list-unstyled px-3"><a href="#">COLLECTIBLES</a></li>
+            <li class="list-unstyled px-3"><a href="#">VIDEOS</a></li>
+            <li class="list-unstyled px-3"><a href="#">FANS</a></li>
+            <li class="list-unstyled px-3"><a href="#">NEWS</a></li>
+            <li class="list-unstyled px-3"><a href="#">SHOP</a></li>
+        </ul>
+        
+    </div>
+    <div class="hero">
+        {{-- <img class="img-fluid" src="{{Vite::asset("resources/img/jumbotron.jpg")}}" alt="hero"> --}}
+    </div>
+</header>
