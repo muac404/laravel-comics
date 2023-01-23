34d3da16de91c743fcd704ed200fa328d656f175
muac404 <muac404@gmail.com>
2023-01-23 16:42:14 +0100

    Aggiunge pagina fumetti

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,8 +4,17 @@
 @endsection
 
 @section("page-content")
-<section>
-    <h2></h2>
+<section class="mt-5">
+    <div class="row">
+        @foreach ($comics as $comic)
+            <div class="col-md-2">
+                <img src="{{$comic["thumb"]}}" alt="{{$comic["title"]}}">
+                <h4>{{$comic["title"]}}</h4>
+                <h5>{{$comic["price"]}}</h5>
+            </div>
+        @endforeach
+        
+    </div>
 </section>
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+    $comics = config("comics");
+    return view('homepage', compact("comics"));
 });
 
 
